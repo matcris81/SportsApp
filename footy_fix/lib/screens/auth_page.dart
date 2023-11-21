9bc2b3f3d9f157737b2d13bdeef95e4a8ebc318f
Matei Crisea <nrs3007@autuni.ac.nz>
2023-11-21 13:59:18 +1300

    facebook auth compiles on ios but doesnt work

---
+++ footy_fix/lib/screens/auth_page.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:footy_fix/screens/login_page.dart';
+import 'package:footy_fix/screens/home.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // user is logged in
+          // if (snapshot.hasData) {
+          //   return const HomeScreen();
+          // }
+
+          // user is NOT logged in
+          // else {
+          return LoginPage();
+          // }
+        },
+      ),
+    );
+  }
+}
--- footy_fix/lib/services/auth_service.dart
+++ footy_fix/lib/services/auth_service.dart
@@ -1,5 +1,7 @@
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class AuthService {
   signInWithGoogle() async {
@@ -9,6 +11,9 @@ class AuthService {
     //obtain auth details from request
     final GoogleSignInAuthentication gAuth = await gUser!.authentication;
 
+    print("Google Sign-In Email: ${gUser.email}");
+    print("Google Sign-In Email: ${gUser.displayName}");
+
     // create a new credential for user
     final credential = GoogleAuthProvider.credential(
       accessToken: gAuth.accessToken,
@@ -17,5 +22,28 @@ class AuthService {
 
     //finally, lets sign in
     return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  signInWithApple() async {
+    try {
+      return await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+      );
+    } catch (error) {
+      print("Error signing in with Apple: $error");
+    }
+  }
+
+  signInWithFacebook() async {
+    FacebookAuth.instance
+        .login(permissions: ["public_profile", "email"]).then((value) {
+      FacebookAuth.instance.getUserData().then((userData) {
+        print("Facebook Sign-In Email: ${userData["email"]}");
+        print("Facebook Sign-In Name: ${userData["name"]}");
+      });
+    });
   }
 }
