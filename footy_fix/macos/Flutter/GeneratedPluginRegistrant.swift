ef6e53ee8295969b61a2a7e984cbeb80c8d177e7
Matei Crisea <nrs3007@autuni.ac.nz>
2023-12-04 17:23:38 +1300

    added game description and address takes you to google maps

--- footy_fix/macos/Flutter/GeneratedPluginRegistrant.swift
+++ footy_fix/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,6 +14,7 @@ import geolocator_apple
 import path_provider_foundation
 import shared_preferences_foundation
 import sign_in_with_apple
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
@@ -25,4 +26,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
