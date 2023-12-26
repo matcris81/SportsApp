99ae9d3960721d72d333653df98b3164489215fc
Matei Crisea <nrs3007@autuni.ac.nz>
2023-12-27 02:01:35 +1300

    moved database to aws

--- footy_fix/lib/services/db_services.dart
+++ footy_fix/lib/services/db_services.dart
@@ -16,18 +16,38 @@ class PostgresService {
     try {
       connection = await Connection.open(
         Endpoint(
-          host: '192.168.3.11',
-          database: 'football_fix',
-          username: 'mat',
-          password: 'pass',
+          host: 'postgres-1.clymy8qkkowx.ap-southeast-2.rds.amazonaws.com',
+          database: 'postgres',
+          username: 'postgres',
+          password: 'Lewandowski9',
         ),
         // The postgres server hosted locally doesn't have SSL by default. If you're
         // accessing a postgres server over the Internet, the server should support
         // SSL and you should swap out the mode with `SslMode.verifyFull`.
-        settings: ConnectionSettings(sslMode: SslMode.disable),
+        settings: const ConnectionSettings(sslMode: SslMode.require),
       );
 
       print('Connected to the db');
+
+      connection!.execute(
+          "CREATE TABLE games (game_id SERIAL PRIMARY KEY, venue_id INTEGER, sport_id INTEGER, "
+          "game_date DATE NOT NULL, start_time TIME WITHOUT TIME ZONE NOT NULL, description VARCHAR(255), "
+          "max_players INTEGER NOT NULL, current_players INTEGER DEFAULT 0, price DOUBLE PRECISION)");
+
+      connection!.execute(
+          "CREATE TABLE sports (sport_id SERIAL PRIMARY KEY, name VARCHAR(255))");
+
+      connection!.execute(
+          "CREATE TABLE users (user_id VARCHAR PRIMARY KEY, username VARCHAR(255), email VARCHAR(255), sport_id INTEGER, venue_id INTEGER)");
+
+      connection!.execute(
+          "CREATE TABLE user_game_participation (participation_id SERIAL PRIMARY KEY, user_id VARCHAR, game_id INTEGER, status VARCHAR(255))");
+
+      connection!.execute(
+          "CREATE TABLE user_likes (user_id VARCHAR NOT NULL, likeable_id INTEGER NOT NULL, likeable_type VARCHAR NOT NULL)");
+
+      connection!.execute(
+          "CREATE TABLE venues (venue_id SERIAL PRIMARY KEY, name VARCHAR(255), address VARCHAR(255), description TEXT)");
     } on SocketException catch (e) {
       // Handle network-related issues here
       print('Network error: $e');
