a7b6b280c0b6b76ef2e3d026eb40a661965c0c4d
Matei Crisea <nrs3007@autuni.ac.nz>
2024-02-20 16:14:55 +1300

    fixed amount to pay bug, removed tiles in more_info screen

--- footy_fix/lib/screens/navigation_screens/more_info.dart
+++ footy_fix/lib/screens/navigation_screens/more_info.dart
@@ -26,28 +26,28 @@ class _ProfileScreenState extends State<ProfileScreen> {
         ),
         body: ListView(
           children: <Widget>[
-            ListTile(
-                leading: const Icon(Icons.account_circle_outlined),
-                title: const Text('Account'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const AccountScreen()),
-                  );
-                }),
-            ListTile(
-                leading: const Icon(Icons.photo_album),
-                title: const Text('Wallet'),
-                onTap: () {}),
-            ListTile(
-                leading: const Icon(Icons.settings),
-                title: const Text('Settings'),
-                onTap: () {}),
-            ListTile(
-                leading: const Icon(Icons.info_outline),
-                title: const Text('About'),
-                onTap: () {}),
+            // ListTile(
+            //     leading: const Icon(Icons.account_circle_outlined),
+            //     title: const Text('Account'),
+            //     onTap: () {
+            //       Navigator.push(
+            //         context,
+            //         MaterialPageRoute(
+            //             builder: (context) => const AccountScreen()),
+            //       );
+            //     }),
+            // ListTile(
+            //     leading: const Icon(Icons.photo_album),
+            //     title: const Text('Wallet'),
+            //     onTap: () {}),
+            // ListTile(
+            //     leading: const Icon(Icons.settings),
+            //     title: const Text('Settings'),
+            //     onTap: () {}),
+            // ListTile(
+            //     leading: const Icon(Icons.info_outline),
+            //     title: const Text('About'),
+            //     onTap: () {}),
             ListTile(
                 leading: const Icon(Icons.logout),
                 title: const Text('Sign out'),
@@ -61,10 +61,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     MaterialPageRoute(builder: (context) => const LoginPage()),
                   );
                 }),
-            ListTile(
-                leading: const Icon(Icons.delete_forever_sharp),
-                title: const Text('Delete account'),
-                onTap: () {}),
+            // ListTile(
+            //     leading: const Icon(Icons.delete_forever_sharp),
+            //     title: const Text('Delete account'),
+            //     onTap: () {}),
           ],
         ),
       ),
